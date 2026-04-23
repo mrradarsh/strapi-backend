66ee778882b26a7d9311aea89d69441f2b786b12
mrradarsh <mrradarsh@users.noreply.github.com>
2026-04-23 15:40:01 +0530

    Add crash logging

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-const { createStrapi } = require('@strapi/strapi');
+const fs = require('fs');
 const path = require('path');
-createStrapi({ appDir: __dirname, distDir: path.join(__dirname, 'dist') }).start();
+
+process.on('uncaughtException', (err) => {
+    fs.writeFileSync(path.join(__dirname, 'crash.log'), err.toString() + '\n' + err.stack);
+});
+
+try {
+    const { createStrapi } = require('@strapi/strapi');
+    createStrapi({ appDir: __dirname, distDir: path.join(__dirname, 'dist') })
+      .start()
+      .catch(err => {
+          fs.writeFileSync(path.join(__dirname, 'crash-promise.log'), err.toString() + '\n' + err.stack);
+      });
+} catch (error) {
+    fs.writeFileSync(path.join(__dirname, 'crash-sync.log'), error.toString() + '\n' + error.stack);
+}
